083bbef5a0447425222f3aa2a038f07a59223241
Justin Gomez <programjustin@gmail.com>
2017-05-12 09:01:04 -0700

    add size option to pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,25 @@ function Pizza(size, toppingOne) {
   this.size = size;
   this.toppingOne = toppingOne;
 }
+Pizza.prototype.size = function (size) {
+  this
+}
 
 $(document).ready(function() {
 
 $("form").submit(function(event) {
   event.preventDefault();
 
+t = $(".pieSize").val();
+alert(this.size);
+
+
 var toppingCheese = $("#chz").val();
 var toppingRoni = $("#roni").val();
 var toppingPeppers = $("#bells").val();
 
 var newOrder = new Pizza(toppingCheese,toppingRoni, toppingPeppers);
-alert(newOrder);
+alert(newOrder.toppingCheese);
 
   });
 });

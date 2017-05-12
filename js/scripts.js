665b9c28bcb770cd01ab54e1501909afd1078b26
Justin Gomez <programjustin@gmail.com>
2017-05-12 14:11:49 -0700

    add a background to he container fluid class

--- js/scripts.js
+++ js/scripts.js
@@ -1,113 +1,79 @@
+//BACK-END LOGIC
 function Pizza(size, topsOne, topsTwo, topsThree) {
   this.size = size;
   this.topsOne = topsOne;
   this.topsTwo = topsTwo;
   this.topsThree = topsThree;
 }
+    //PIZZA PROTOTYPE THAT CALCULATES THE COST OF A PIZZA
+
 Pizza.prototype.cost = function(size, topsOne, topsTwo, topsThree) {
 
     //COST FOR SMALL PIZZA
+
    if(size === "Small" && topsOne === "Feta" && topsTwo === "Pepperoni" && topsThree === "Peppers") {
      return 16;
 
     } else if(size === "Small" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers") && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers")) {
-
        return  14;
-
-     } else if (size === "Small" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers"))  {
-
+    } else if (size === "Small" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers"))  {
        return 12;
-
     } else if (size === "Small") {
-
       return 11;
-
       //COST FOR MEDIUM PIZZA
-
     } else if(size === "Medium" && topsOne === "Feta" && topsTwo === "Pepperoni" && topsThree === "Peppers") {
       return 20;
-
     } else if(size === "Medium" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers") && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers")) {
-
         return  17;
-
       } else if (size === "Medium" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers"))  {
-
         return 15;
-
      } else if (size === "Medium") {
        return 14;
-
        //COST FOR LARGE PIZZAS
      } else if(size === "Large" && topsOne === "Feta" && topsTwo === "Pepperoni" && topsThree === "Peppers") {
        return 24;
-
      } else if(size === "Large" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers") && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers")) {
-
          return  20;
-
        } else if (size === "Large" && (topsOne === "Feta" || topsTwo === "Pepperoni" || topsThree === "Peppers"))  {
-
          return 18;
-
       } else if (size === "Large") {
         return 17;
       }
 }
-
-
-
-
+//FRONT-END LOGIC
 $(document).ready(function() {
-
 $("form").submit(function(event) {
   event.preventDefault();
+  this.size = $("input:radio:checked").val();
+  var toppingCheese = $("input:checkbox[name=Feta]:checked").val();
+  var toppingRoni = $("input:checkbox[name=Pepperoni]:checked").val();
+  var toppingPeppers = $("input:checkbox[name=Pepps]:checked").val();
 
-this.size = $("input:radio:checked").val();
-
-
-var toppingCheese = $("input:checkbox[name=Feta]:checked").val();
-var toppingRoni = $("input:checkbox[name=Pepperoni]:checked").val();
-var toppingPeppers = $("input:checkbox[name=Pepps]:checked").val();
-
-var newOrder = new Pizza(this.size, toppingCheese, toppingRoni, toppingPeppers);
-
-newOrder.price = newOrder.cost(this.size, toppingCheese, toppingRoni, toppingPeppers);
-
+  var newOrder = new Pizza(this.size, toppingCheese, toppingRoni, toppingPeppers);
+  //THIS CALLS THE METHOD THAT CAULCATES THE COST OF A PIZZA
+  newOrder.price = newOrder.cost(this.size, toppingCheese, toppingRoni, toppingPeppers);
 
   if (newOrder.size === "Small") {
-
       if (newOrder.topsOne === undefined) {
-
         newOrder.topsOne = "-";
-
       }
-       if (newOrder.topsTwo === undefined) {
-
+      if (newOrder.topsTwo === undefined) {
         newOrder.topsTwo = "-";
-
       }
-       if (newOrder.topsThree === undefined) {
-
+      if (newOrder.topsThree === undefined) {
         newOrder.topsThree = "-";
       }
-
       $(".col-md-6").append("<h1>" + newOrder.price + "</h1>" + "<h1>" + newOrder.topsOne + "</h1>" + "<h1>" + newOrder.topsTwo + "</h1>" + "<h1>" + newOrder.topsThree + "</h1>");
-
-    } else if (newOrder.size === "Medium") {
+  } else if (newOrder.size === "Medium") {
 
 
     if (newOrder.topsOne === undefined) {
         newOrder.topsOne = "-";
       }
-
     if (newOrder.topsTwo === undefined) {
-
         newOrder.topsTwo = "-";
-
       }
     if (newOrder.topsThree === undefined) {
-
         newOrder.topsThree = "-";
       }
 
@@ -116,34 +82,19 @@ newOrder.price = newOrder.cost(this.size, toppingCheese, toppingRoni, toppingPep
   } else if (newOrder.size === "Large") {
 
       if (newOrder.topsOne === undefined) {
-        
         newOrder.topsOne = "-";
       }
       if (newOrder.topsTwo === undefined) {
-
         newOrder.topsTwo = "-";
-
       }
       if (newOrder.topsThree === undefined) {
-
-        newOrder.topsThree = "-";
+          newOrder.topsThree = "-";
       }
 
       $(".col-md-6").append("<h1>" + newOrder.price + "</h1>" + "<h1>" + newOrder.topsOne + "</h1>" + "<h1>" + newOrder.topsTwo + "</h1>" + "<h1>" + newOrder.topsThree + "</h1>");
-
+  } else {
+    alert("PLEASE TRY AGAIN");
   }
-
-
-   else {
-    alert("wrong");
-  }
-
-  // if (newOrder.topsOne === undefined) {
-  //   $("#priceHide").hide();
-  // }
-  // $("#priceHide").hide();
-
-
 console.log(newOrder);
 
   });
